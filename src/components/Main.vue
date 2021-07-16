a4639ad0db5b16f3a72128dfbc00a5d1fc6db137
Francesco <francescococlite@gmail.com>
2021-07-16 12:39:11 +0200

    add fontawsome and bootstrap

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+   <i class="fas fa-arrow-right text-primary"></i>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
